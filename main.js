83d91e85a7b7dc711539c7067682c2bf64a414cf
puthpiseth <puthpiseth_tun@yahoo.com>
2021-02-15 14:48:10 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Afficher la liste des Pokémons (20)
+// Quand j'appuie sur next, la liste se met à jour avec les 20 Pokémons suivants (s'il y en a)
+// Quand j'appuie sur prev, la liste se met à jour avec les 20 Pokémons précédents (s'il y en a)
+// Quand je clique sur le nom d'un Pokémon, le Pokédex affiche les informations du Pokémon concerné
+// La couleur de fond s'adapte au type du Pokémon : type Plante > Vert, type Feu > Orange, etc.
+// Quand le Pokémon a un seul type, afficher un seul type, quand il en a deux, affichers les deux
+
+
+// Ça peut être utile de stocker l'url "précédente" et l'url "suivante".
+
+// Les différents types : ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock', 'bug', 'ghost', 'steel', 'fire', 'water', 'grass', 'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy']
+
+// lien de pokeDex
+"https://pokeapi.co/"
